800bbfef5b036237c5f9de2a250e15c6f8f4fefd
Bayekula-hb <hobedbayekula@gmail.com>
2021-04-09 12:26:46 +0100

    Responsive gérer sauf le bouton du menu

--- Js/app.js
+++ Js/app.js
@@ -68,23 +68,19 @@ let btn__contact = document.querySelector(".btn--contact--menu");
 let section = document.querySelector(".section--div--4--hidden");
 let btn__fermer = document.querySelector(".btn--close");
 
-function add_class (name__element,name__classe){
-    name__element.classList.add(name__classe);
-}
-function delette_classe (name__element,name__classe){
-    name__element.classList.remove(name__classe);
-}
+
 btn__contact.addEventListener("click", function(event){
     event.preventDefault();
-    section.classList.add("block--view");
+    section.classList.toggle("block--view");
     //section.style.display = "flex";
-   //section.setAttribute("class", "block--view");
-   //add_class(section,"block--view");
+    //section.setAttribute("class", "block--view");
+    //add_class(section,"block--view");
 });
 btn__fermer.addEventListener("click", function(event){
     event.preventDefault();
-    section.classList.remove("block--view");
+    section.classList.toggle("block--view");
     //delette_classe(section, "block--view");
     //document.querySelector(".section--div--4--hidden").style.display = "none";
 })
 console.log(btn__contact);
+console.log(section);
